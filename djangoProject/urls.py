f7260dcf84e588b3d607d72f83e7b642a1245ec8
Zehuan Huang <zewanhuang@126.com>
2021-08-19 19:34:57 +0800

    修改部署后端路由

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -41,9 +41,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     # drf_yasg
     re_path('^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-spec'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/pt/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/pt/docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    path('admin/', admin.site.urls),
-    path('user/', include(('userinfo.urls', 'userinfo'))),
+    path('api/pt/admin/', admin.site.urls),
+    path('api/pt/user/', include(('userinfo.urls', 'userinfo'))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
