f9d45d8d0498b9daecd3c1d9ed3c3ac0f935acb9
BFlameSwift <19373732@buaa.edu.cn>
2021-08-21 14:45:46 +0800

    code create_qn

--- Questionnaire/form.py
+++ Questionnaire/form.py
@@ -8,3 +8,10 @@ class QuestionIdForm(forms.Form):
 
 class OptionIdForm(forms.Form):
     option_id = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+class CreateNewQnForm(forms.Form):
+    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    title = forms.CharField(label="标题", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    subtitle = forms.CharField(label="副标题", max_length=256, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # direction = forms.CharField(label="简介", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    type = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
