1d88f9c43e203cac51ebf5b921842e47b24f3920
shiyiting753 <19373753@buaa.edu.cn>
2021-08-21 16:35:34 +0800

    返回问卷

--- Qn/views.py
+++ Qn/views.py
@@ -106,7 +106,7 @@ def get_list(request):
                      operation_summary='统计问卷回收量',
                      operation_description="返回每日的问卷回收量",
                      manual_parameters=[Parameter(name='survey_id', in_=IN_QUERY, description='问卷编号',
-                                                type=TYPE_INTEGER, required=True),],
+                                                  type=TYPE_INTEGER, required=True), ],
                      responses=polygon_view_get_resp
                      )
 @api_view(['GET'])
@@ -134,3 +134,62 @@ def get_recycling_num(request):
             json_item = {"date": date, "number": number}
             json_list.append(json_item)
         return JsonResponse(list(json_list), safe=False, json_dumps_params={'ensure_ascii': False})
+
+
+@csrf_exempt
+@swagger_auto_schema(method='get',
+                     tags=['问卷相关'],
+                     operation_summary='返回答卷',
+                     operation_description="返回用户的所有答案",
+                     manual_parameters=[Parameter(name='survey_id', in_=IN_QUERY, description='问卷编号',
+                                                  type=TYPE_INTEGER, required=True),
+                                        Parameter(name='username', in_=IN_QUERY, description='问卷编号',
+                                                  type=TYPE_STRING, required=True)],
+                     responses=polygon_view_get_resp
+                     )
+@api_view(['GET'])
+def get_answer(request):
+    # 检验是否登录
+    global answer_questions
+    if not request.session.get('is_login'):
+        return JsonResponse({'status_code': 401})
+
+    if request.method == 'GET':
+        survey_id = request.GET.get('survey_id')
+        username = request.GET.get('username')
+
+        # 用户名是否匹配
+        if username != request.session.get('username'):
+            return JsonResponse({'status_code': 403})
+
+        # 问卷信息
+        result = {}
+        try:
+            survey = Survey.objects.get(survey_id=survey_id)
+            if username != survey.username:
+                return JsonResponse({'status_code': 403})
+        except:
+            return JsonResponse({'status_code': 402})
+        result['survey_id'] = survey_id
+        result['title'] = survey.title
+        result['subtitle'] = survey.subtitle
+        result['username'] = username
+
+        # 回答信息
+        result_answers = []
+        questions = Question.objects.filter(survey_id=survey).order_by('sequence')
+        submits = Submit.objects.filter(survey_id=survey)
+
+        for submit in submits:
+            answer_questions = []
+            for question in questions:
+                answer_question = {"question_id": question.id, "sequence": question.sequence,
+                                   "title": question.title, "direction": question.direction,
+                                   "is_must_answer": question.is_must_answer, "type": question.type}
+                answer = Answer.objects.get(question_id=question, submit_id=submit)
+                answer_question['answer'] = answer
+                answer_questions.append(answer_question)
+            result_answers.append(answer_questions)
+
+        result['answers'] = result_answers
+        return JsonResponse(result)
