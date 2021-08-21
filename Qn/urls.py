ca9b63b019edb38465cc8e638ffe38240e7a1720
BFlameSwift <19373732@buaa.edu.cn>
2021-08-21 21:38:32 +0800

    merge and fix bugs

--- Qn/urls.py
+++ Qn/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
 
     path('delete/qn/not_real/',delete_survey_not_real),
     path('delete/qn/real/',delete_survey_real),
+    path('delete/qn/empty/',empty_the_recycle_bin),
     path('delete/question/',delete_question),
     path('delete/option/',delete_option),
+
 
 
     path('get/qn_detail/',get_survey_details),
