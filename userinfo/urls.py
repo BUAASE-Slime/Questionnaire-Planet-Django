0adbff57abe120caf830c771736fd8ea30396c28
huanngzh <huanngzh@gmail.com>
2021-08-22 13:48:00 +0800

    delete the same part

--- userinfo/urls.py
+++ userinfo/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import *
+
+
 urlpatterns = [
     path('login', login),
     path('register', register),
     path('logout', logout),
-    path('confirm/', user_confirm),
+    path('confirm', user_confirm),
     path('unverified_email', unverified_email),
 
 ]
