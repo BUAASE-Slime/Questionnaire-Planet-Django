ba3c6382608d0904ae11b12f7a2cfad98de6f30a
Zehuan Huang <zewanhuang@126.com>
2021-08-28 02:00:36 +0800

    疫情问卷

--- epidemic/urls.py
+++ epidemic/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('create_qn', create_qn_epidemic),
     path('save_ans', save_epidemic_answer),
+    path('save_ans_by_code', save_epidemic_answer_by_code),
     path('upload_img', upload_image),
     path('get_img', get_image_url),
     path('upload_video', upload_video),
