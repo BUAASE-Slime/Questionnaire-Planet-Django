50216ba3f973edda752d0b3ff933305696557fb5
BFlameSwift <19373732@buaa.edu.cn>
2021-08-27 20:45:21 +0800

    ip blast_question

--- Submit/views.py
+++ Submit/views.py
@@ -1644,7 +1644,13 @@ def save_question_by_order(qn,question_order):
         i += 1
     return 0
 
-
-
+@csrf_exempt
+def get_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]#所以这里是真实的ip
+    else:
+        ip = request.META.get('REMOTE_ADDR')#这里获得代理ip
+    return JsonResponse({'ip':ip})
 
 
