c5140dec1a8edf01e6e171d99c934fa4ac70f358
shiyiting753 <19373753@buaa.edu.cn>
2021-08-27 16:59:31 +0800

    上传图片

--- epidemic/urls.py
+++ epidemic/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('create_qn', create_qn_epidemic),
-    path('save_ans', save_epidemic_answer)
+    path('save_ans', save_epidemic_answer),
+    path('upload_img',upload_image),
 ]
